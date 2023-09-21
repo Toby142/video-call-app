b9dd4834ea634e60e91217906e913573240c71ec
Toby142 <tobiasvandeven142@gmail.com>
2023-09-21 14:55:49 +0200

    firebase installed

--- src/app/call-page/call-page.component.ts
+++ src/app/call-page/call-page.component.ts
@@ -1,10 +1,25 @@
 import { Component } from '@angular/core';
 
+import * as firebase from 'firebase/app';
+import 'firebase/firestore';
+
 @Component({
   selector: 'app-call-page',
   templateUrl: './call-page.component.html',
   styleUrls: ['./call-page.component.css']
 })
 export class CallPageComponent {
+
+  startWebcam() {
+    const video = document.getElementById('webcam') as HTMLVideoElement;
+
+    navigator.mediaDevices.getUserMedia({
+      video: true,
+      audio: true
+    }).then(stream => {
+      video.srcObject = stream;
+      video.play();
+    });
+  }
 
 }
